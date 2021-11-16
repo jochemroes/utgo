d4c8963ab3d9f2bd4d5035f98ddf61659f63deff
Remco van Dijk <r.vandijk@student.utwente.nl>
2021-11-16 22:53:33 +0100

    automatic crash reports met ACRA, kankerbugs

---
+++ app/src/main/java/nl/utwente/utgo/UtgoApp.java
@@ -0,0 +1,18 @@
+package nl.utwente.utgo;
+
+import android.app.Application;
+import android.content.Context;
+
+
+public class UtgoApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+    }
+}
